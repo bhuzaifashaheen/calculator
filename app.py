bbb403e87df085cf1cfe70ec07a1a5951a5fde58
bhuzaifashaheen <bhuzaifashaheen@gmail.com>
2026-02-09 18:00:42 +0530

    Port 0000

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from logic.calculator import calculate
-
+import os
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
@@ -18,5 +18,6 @@ def index():
 
     return render_template("index.html", result=result)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":      
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
